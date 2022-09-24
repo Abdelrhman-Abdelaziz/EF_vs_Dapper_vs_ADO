637ef78a3f08776eb91b5a1d690fe66a049d23ca
Abdelrhman Abdelaziz <AbdelrhmanhAbdelaziz@gmail.com>
2022-09-24 15:50:06 +0200

    Add project files.

---
+++ EF_vs_Dapper_vs_ADO.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EF_vs_Dapper_vs_ADO.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
