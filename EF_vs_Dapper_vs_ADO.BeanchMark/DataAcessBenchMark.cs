c087569f5f2605df28bd0ba22c91798ec5e2974b
Abdelrhman Abdelaziz <AbdelrhmanhAbdelaziz@gmail.com>
2022-09-25 23:38:06 +0200

    Add BenchMark project

---
+++ EF_vs_Dapper_vs_ADO.BeanchMark/DataAcessBenchMark.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+using EF_vs_Dapper_vs_ADO.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_vs_Dapper_vs_ADO.BenchMark
+{
+    public class DataAcessBenchMark
+    {
+        [Benchmark]
+        public  void GetAllEmployeesUsingEF()
+        {
+            IUnitOfWork unitOfWork = new EF.Repositories.UnitOfWork(new EF.AppDBContext());
+            var _ = unitOfWork.Employees.GetAllAsync().Result;
+            Console.WriteLine($"Number of Employees is {_?.Count()}");
+        }
+        [Benchmark]
+        public void GetAllEmployeesUsingDapper()
+        {
+            IUnitOfWork unitOfWork = new Dapper.UnitOfWork();
+            var _ = unitOfWork.Employees.GetAllAsync().Result;
+            Console.WriteLine($"Number of Employees is {_?.Count()}");
+        }
+        [Benchmark]
+        public void GetAllEmployeesUsingADO()
+        {
+            IUnitOfWork unitOfWork = new ADO.UnitOfWork();
+            var _ = unitOfWork.Employees.GetAllAsync().Result;
+            Console.WriteLine($"Number of Employees is {_?.Count()}");
+
+        }
+
+    }
+}
---
+++ EF_vs_Dapper_vs_ADO.BeanchMark/Program.cs
@@ -0,0 +1,5 @@
+﻿using BenchmarkDotNet.Running;
+using EF_vs_Dapper_vs_ADO.BenchMark;
+
+
+var summary = BenchmarkRunner.Run<DataAcessBenchMark>();
